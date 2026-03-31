59c0f7ef61284fcbf4fd6f78dd59c384046ae9c7
d11416146 <d11416146@gs.takming.edu.tw>
2026-03-31 15:28:05 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//父類別
+public class person
+{
+    public string Name="姓氏";
+
+    //建構子
+	public person(string n)
+	{
+        Name= n;
+	}
+}
+//子類別
+public class Student :person
+{
+    public string stdname="名字";
+    //建構子
+    public Student(string n):base("陳")
+    {
+        stdname=Name+n;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace class_0331_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("咚咚");
+            Console.WriteLine(s.Name);
+            Console.WriteLine(s.stdname);
+        }
+    }
+}
